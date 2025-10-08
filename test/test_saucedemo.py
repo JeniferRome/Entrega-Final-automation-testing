8392ce7dd9536db35e461fff0e2ec34eac8729f9
Jenifer <jeniferromeroerazo@gmail.com>
2025-10-08 17:26:33 -0400

    subida de los primeros archivos de la preEntrega

---
+++ test/test_saucedemo.py
@@ -0,0 +1,37 @@
+import pytest
+
+
+
+
+@pytest.fixture
+def dirver():
+    # configuracion para conultar a selenium
+
+
+def test_login():
+    # logeo de usuario con username y password
+    # click al boton de login
+
+    # redirigir a la pagina de inventario
+
+    # verificar el titulp de la pagina(ventanita)
+
+def test_catalogo():
+    # logeo de usuario con username y password
+    # click al boton de login
+
+    
+    #podamos verificar el titulo pero del html
+    
+#comprobar si existen productos en la pagina visibles(len())
+#verificar elementos importantes de la pagina
+
+
+def test_carrito():
+# logeo de usuario con username y password
+# click al boton de login
+#llevarme a la pagina del carrito de compras
+#incremento del carrito al agregar un producto
+
+#comprobar que en el carrito aparezca el producto correctamente
+
